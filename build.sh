cc9c817866e7af75ff9991024198a9fa017a1b41
Nagol12344 <63481547+Nagol12344@users.noreply.github.com>
2025-08-05 16:51:09 -0400

    Building swarmfm player

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# OFFICAL APT-SWARM BUILD SCRIPT
+# MAINTAINER: Nagol12344 
+# THIS SCRIPT IS MADE FOR GITHUB ACTIONS, IT DOESNT CARE ABOUT YOUR SYSTEM AND WILL JUST INSTAL PACKAGES 
+# YOU HAVE BEEN WARNED
+
+sudo apt-get update
+sudo apt-get install -y libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev
+
+cd build
+
+cargo install tauri-cli --version "^2.0.0" --locked
+cargo tauri build
+
+mkdir -p ../built
+cp -r src-tauri/target/release/bundle/deb/*.deb ../built/
